74040722d1c7f75a959ae5c6013769800a973d54
Dillon Dixon <dillondixon@gmail.com>
2013-07-29 12:09:43 -0400

    Fixed a bug in which the output may not be 16-bit aligned under some circumstances. Also fixed text formatting and indentation in the source code.

--- src/lzstring/exceptions/DecompressException.java
+++ src/lzstring/exceptions/DecompressException.java
@@ -2,12 +2,12 @@ package lzstring.exceptions;
 
 public class DecompressException extends Exception {
 
-  private static final long serialVersionUID = 207894408656310263L;
+	private static final long serialVersionUID = 207894408656310263L;
 
-  public DecompressException(String message, Exception e){
+	public DecompressException(String message, Exception e){
 		super(message, e);
 	}
-	
+
 	public DecompressException(String message){
 		super(message);
 	}
--- src/lzstring/streaming/BitOutputStream.java
+++ src/lzstring/streaming/BitOutputStream.java
@@ -4,36 +4,38 @@ import java.io.IOException;
 
 public class BitOutputStream {
 
-  ByteArrayOutputStream bos = new ByteArrayOutputStream();
-  
-  private int pos = 0, bytesWritten = 0;
-  private byte thisByte = 0;
-  
-  public byte[] getContent() throws IOException {
-  
-    while(thisByte != 0)
-      writeBits(1, 0);
-    
-    if(bytesWritten % 2 != 0)
-      bos.write((byte)0);
-    
-    return bos.toByteArray();
-  }
-  
-  public void writeBits(int numBits, int value) throws IOException {
-    
-    for(int i = 0; i < numBits; i++){
-      
-      thisByte = (byte) ((thisByte << 1) | (value & 1));
-      
-      if(++pos % 8 == 0){
-        bos.write(thisByte);
-        pos = 0;
-      }
-      
-      value >>= 1;
-    }
-    
-    bytesWritten += numBits;
-  }
+	ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+	private int pos = 0, bytesWritten = 0;
+	private byte thisByte = 0;
+
+	public byte[] getContent() throws IOException {
+
+		// Replicate the 16-bit behaviour of the JavaScript implementation for terminating the string.
+		while(thisByte != 0)
+			writeBits(1, 0);
+
+		if(bytesWritten % 2 != 0)
+			bos.write((byte)0);
+
+		return bos.toByteArray();
+	}
+
+	public void writeBits(int numBits, int value) throws IOException {
+
+		for(int i = 0; i < numBits; i++){
+
+			thisByte = (byte) ((thisByte << 1) | (value & 1));
+
+			if(++pos % 8 == 0){
+				
+				bos.write(thisByte);
+				pos = 0;
+
+				bytesWritten++;
+			}
+
+			value >>= 1;
+		}
+	}
 }
